b2d427a0e3a9c8e513cde3ccabd7a57078cb9f36
egor <egor.chernukha@gmail.com>
2021-11-23 18:46:27 +0300

    Обновил grafana dashboard

--- data/test.sh
+++ data/test.sh
@@ -4,7 +4,7 @@
 
 while true; do
     # get existent user
-    ab -n 1000 -c 50 http://arch.homework/api/v1/user/6ef3e6a2-4a04-11ec-92cd-0242ac110010
+    ab -n 1000 -c 50 http://arch.homework/api/v1/user/42b96cc5-4c74-11ec-bc9b-0242ac110009
     # get non-existent user
     ERR_COUNT=$((1 + $RANDOM % 50))
     ab -n $ERR_COUNT -c $ERR_COUNT http://arch.homework/api/v1/user/00000000-0000-0000-0000-000000000000
