63dfd449bc688433c8cd7d8f2d7d3f6b6a9cacc6
egor <egor.chernukha@gmail.com>
2022-01-07 18:14:56 +0300

    Имплементация метода получения статуса заказа

--- pkg/order/infrastructure/transport/server.go
+++ pkg/order/infrastructure/transport/server.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/pkg/errors"
+	uuid "github.com/satori/go.uuid"
+	"github.com/sirupsen/logrus"
 
 	"store/pkg/common/infrastructure/jwt"
 
@@ -29,21 +31,28 @@ const (
 
 var errUnauthorized = errors.New("not authorized")
 var errForbidden = errors.New("access denied")
+var errBadRequest = errors.New("bad request")
+
+type userOrderStatusData struct {
+	Status int `json:"status"`
+}
 
 type Server interface {
 	Start()
 }
 
-func NewServer(router *mux.Router, tokenParser jwt.TokenParser) Server {
+func NewServer(router *mux.Router, tokenParser jwt.TokenParser, userOrderQueryService app.UserOrderQueryService) Server {
 	return &server{
-		router:      router,
-		tokenParser: tokenParser,
+		router:                router,
+		tokenParser:           tokenParser,
+		userOrderQueryService: userOrderQueryService,
 	}
 }
 
 type server struct {
-	router      *mux.Router
-	tokenParser jwt.TokenParser
+	router                *mux.Router
+	tokenParser           jwt.TokenParser
+	userOrderQueryService app.UserOrderQueryService
 }
 
 type errorInfo struct {
@@ -52,57 +61,111 @@ type errorInfo struct {
 }
 
 func (s *server) Start() {
-	s.router.HandleFunc(createOrderEndpoint, s.createOrderEndpoint).Methods(http.MethodPost)
-	s.router.HandleFunc(cancelOrderEndpoint, s.cancelOrderEndpoint).Methods(http.MethodPost)
-	s.router.HandleFunc(getOrderStatusEndpoint, s.getOrderStatusEndpoint).Methods(http.MethodGet)
-	s.router.HandleFunc(listOrdersEndpoint, s.listOrdersEndpoint).Methods(http.MethodGet)
+	s.router.Methods(http.MethodPost).Path(createOrderEndpoint).Handler(s.makeHandlerFunc(s.createOrderEndpoint))
+	s.router.Methods(http.MethodPost).Path(cancelOrderEndpoint).Handler(s.makeHandlerFunc(s.cancelOrderEndpoint))
+	s.router.Methods(http.MethodGet).Path(getOrderStatusEndpoint).Handler(s.makeHandlerFunc(s.getOrderStatusEndpoint))
+	s.router.Methods(http.MethodGet).Path(listOrdersEndpoint).Handler(s.makeHandlerFunc(s.listOrdersEndpoint))
 }
 
-func (s *server) createOrderEndpoint(w http.ResponseWriter, r *http.Request) {
+func (s *server) makeHandlerFunc(handler func(http.ResponseWriter, *http.Request) error) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		_ = request.ParseForm()
+		fields := logrus.Fields{
+			"method": request.Method,
+			"host":   request.Host,
+			"path":   request.URL.Path,
+		}
+		if request.URL.RawQuery != "" {
+			fields["query"] = request.URL.RawQuery
+		}
+		if request.PostForm != nil {
+			fields["post"] = request.PostForm
+		}
+
+		err := handler(writer, request)
+
+		if err != nil {
+			writeErrorResponse(writer, err)
+
+			fields["err"] = err
+			logrus.WithFields(fields).Error(err)
+		} else {
+			logrus.WithFields(fields).Info("call")
+		}
+	}
+}
+
+func (s *server) createOrderEndpoint(w http.ResponseWriter, r *http.Request) error {
 	tokenData, err := s.extractAuthorizationData(r)
 	if err != nil {
-		writeErrorResponse(w, err)
-		return
+		return err
 	}
 
 	_ = tokenData
+	return nil
 }
 
-func (s *server) cancelOrderEndpoint(w http.ResponseWriter, r *http.Request) {
+func (s *server) cancelOrderEndpoint(w http.ResponseWriter, r *http.Request) error {
 	vars := mux.Vars(r)
 	id := vars["id"]
 
 	tokenData, err := s.extractAuthorizationData(r)
 	if err != nil {
-		writeErrorResponse(w, err)
-		return
+		return err
 	}
 	_ = id
 	_ = tokenData
+
+	return nil
 }
 
-func (s *server) getOrderStatusEndpoint(w http.ResponseWriter, r *http.Request) {
+func (s *server) getOrderStatusEndpoint(w http.ResponseWriter, r *http.Request) error {
 	vars := mux.Vars(r)
 	id := vars["id"]
 
 	tokenData, err := s.extractAuthorizationData(r)
 	if err != nil {
-		writeErrorResponse(w, err)
-		return
+		return err
 	}
 
-	_ = id
-	_ = tokenData
+	orderID, err := uuid.FromString(id)
+	if err != nil {
+		return errBadRequest
+	}
+
+	userOrderData, err := s.userOrderQueryService.FindUserOrderByOrderID(orderID)
+	if err != nil {
+		return err
+	}
+
+	if userOrderData.UserID.String() != tokenData.UserID() {
+		return errForbidden
+	}
+
+	writeResponse(w, userOrderStatusData{Status: userOrderData.Status})
+	return nil
 }
 
-func (s *server) listOrdersEndpoint(w http.ResponseWriter, r *http.Request) {
+func (s *server) listOrdersEndpoint(w http.ResponseWriter, r *http.Request) error {
 	tokenData, err := s.extractAuthorizationData(r)
+	if err != nil {
+		return err
+	}
+
+	_ = tokenData
+
+	return nil
+}
+
+func writeResponse(w http.ResponseWriter, response interface{}) {
+	js, err := json.Marshal(response)
 	if err != nil {
 		writeErrorResponse(w, err)
 		return
 	}
-
-	_ = tokenData
+	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(js)
 }
 
 func writeErrorResponse(w http.ResponseWriter, err error) {
